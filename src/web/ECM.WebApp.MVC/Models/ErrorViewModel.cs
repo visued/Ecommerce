918f4493580c304260bc6e3755852d00b8394e70
Victor Sued <visued@gmail.com>
2020-10-14 18:33:54 -0300

    Start of project

---
+++ src/web/ECM.WebApp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ECM.WebApp.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
