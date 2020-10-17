e09aa76cd2228f1185ba054cb84081510f1134d4
Victor Sued <visued@gmail.com>
2020-10-17 05:02:12 -0300

    adicionado a respoat customizada

--- src/services/ECM.Identitidade.API/Controllers/IdentidadeController.cs
+++ src/services/ECM.Identitidade.API/Controllers/IdentidadeController.cs
@@ -15,7 +15,7 @@ namespace ECM.Identitidade.API.Controllers
 {
     [ApiController]
     [Route("api/v1/identidade")]
-    public class IdentidadeController : Controller
+    public class IdentidadeController : MainController
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
@@ -31,7 +31,7 @@ namespace ECM.Identitidade.API.Controllers
         [HttpPost("nova-conta")]
         public async Task<ActionResult> Registrar(UsuarioRegistro usuarioRegistro)
         {
-            if (!ModelState.IsValid) return BadRequest();
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
 
             var user = new IdentityUser
             {
@@ -45,25 +45,37 @@ namespace ECM.Identitidade.API.Controllers
             if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(user, false);
-                return Ok(await GerarJwt(usuarioRegistro.Email));
+                return CustomResponse(await GerarJwt(usuarioRegistro.Email));
             }
 
-            return BadRequest();
+            foreach (var error in result.Errors)
+            {
+                AdicionarErroProcessamento(error.Description);
+            }
+
+            return CustomResponse();
         }
 
         [HttpPost("autenticar")]
         public async Task<ActionResult> Login(UsuarioLogin usuarioLogin)
         {
-            if (!ModelState.IsValid) return BadRequest();
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
 
             var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha, false, true);
 
             if (result.Succeeded)
             {
-                return Ok(await GerarJwt(usuarioLogin.Email));
+                return CustomResponse(await GerarJwt(usuarioLogin.Email));
             }
 
-            return BadRequest();
+            if (result.IsLockedOut)
+            {
+                AdicionarErroProcessamento("Usuário temporariamente bloqueado por tentativas inválidas.");
+                return CustomResponse();
+            }
+            AdicionarErroProcessamento("Usuário ou senha inválidos.");
+
+            return CustomResponse();
 
         }
 
---
+++ src/services/ECM.Identitidade.API/Controllers/MainController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ECM.Identitidade.API.Controllers
+{
+    [ApiController]
+    public class MainController : Controller
+    {
+        protected ICollection<string> Erros = new List<string>();
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperacaoValida())
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+            {
+                {"Mesagens", Erros.ToArray() }
+
+            }));
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            var erros = modelState.Values.SelectMany(e => e.Errors);
+            foreach (var erro in erros)
+            {
+                AdicionarErroProcessamento(erro.ErrorMessage);
+            }
+
+            return CustomResponse();
+        }
+
+        protected bool OperacaoValida()
+        {
+            return !Erros.Any();
+        }
+
+        protected void AdicionarErroProcessamento(string erro)
+        {
+            Erros.Add(erro);
+        }
+    }
+}
